311416b607f881eddb7d500950fd8e1980d43acd
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2016-04-08 17:54:26 +0200

    resolved #1, make it possible to give length of generated string

--- lib/puppet/parser/functions/pwgen.rb
+++ lib/puppet/parser/functions/pwgen.rb
@@ -1,5 +1,6 @@
 module Puppet::Parser::Functions
-  newfunction(:pwgen, :type => :rvalue) do 
-    `pwgen -N 1`.chomp
+  newfunction(:pwgen, :type => :rvalue) do |args|
+    l = ( args.empty? ? 8 : args[0])
+    `pwgen -N 1 #{l}`.chomp
   end
 end
